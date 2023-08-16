2528b29d2f2152a47eb5885475669d4ba4a5e960
Sinem <sinem.tkgzz@gmail.com>
2023-08-16 13:52:26 +0300

    Frontend Pepsi Web Design

---
+++ js.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+  document.querySelector(".pepsi").src = anything;
+}
+
+function changeColors(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
